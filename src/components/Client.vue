adc3a513c62582193fd92ada3704425a028b9ff3
nancy <smithnancy@outlook.com>
2023-07-04 11:32:07 +1000

    client component link to edit and delete actions added

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,5 +1,6 @@
 <script setup>
     import { computed } from 'vue';
+    import { RouterLink } from 'vue-router';
 
     const props = defineProps({
         client: {
@@ -28,7 +29,15 @@
 
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
-         
+            <RouterLink
+                to="/"
+                class="text-indigo-600 hover:text-indigo-900 mr-5"
+            >Edit</RouterLink>
+            <button
+                class="text-red-600 hover:text-red-900"
+            >
+                Delete
+            </button>
         </td>
     </tr>
 </template>
